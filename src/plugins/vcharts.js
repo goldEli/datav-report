ef0fe2144e52e9f589ff77dd8cd0de558c0bdd41
miaoyu <miaoyu@sefonsoft.com>
2021-01-11 15:34:58 +0800

    map

--- src/plugins/vcharts.js
+++ src/plugins/vcharts.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import VELine from 'v-charts/lib/line.common'
+import BMap from 'v-charts/lib/bmap.common'
+import 'v-charts/lib/style.css'
 
 Vue.component('ve-line', VELine)
+Vue.component('ve-bmap', BMap)
