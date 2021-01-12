d94e9f352675f4f6978affc2edc82adc3f24e966
miaoyu <miaoyu@sefonsoft.com>
2021-01-12 12:31:01 +0800

    eslint

--- src/components/BMapScatter/index.vue
+++ src/components/BMapScatter/index.vue
@@ -5,31 +5,30 @@
     :tooltip="chartTooltip"
     :title="title"
     height="100%"
-  >
-  </ve-bmap>
+  ></ve-bmap>
 </template>
 
 <script>
-import { data, geo, styleJson } from "./data";
+import { data, geo, styleJson } from './data'
 const convertData = function (data, geo) {
-  const res = [];
+  const res = []
   data.forEach((item) => {
-    const { name, value } = item;
-    const coord = geo[name];
+    const { name, value } = item
+    const coord = geo[name]
     res.push({
       name,
       value: [...coord, value],
-    });
-  });
-  return res;
-};
+    })
+  })
+  return res
+}
 
 export default {
   data() {
-    this.chartTooltip = { show: true };
+    this.chartTooltip = { show: true }
     return {
       chartSettings: {
-        key: "oBvDtR6nzWtVchkY4cLHtnah1VVZQKRK",
+        key: 'oBvDtR6nzWtVchkY4cLHtnah1VVZQKRK',
         bmap: {
           center: [104.114129, 37.550339],
           zoom: 5,
@@ -42,23 +41,23 @@ export default {
       chartTooltip: {},
       chartSeries: [
         {
-          name: "销售额",
-          type: "scatter",
-          coordinateSystem: "bmap",
+          name: '销售额',
+          type: 'scatter',
+          coordinateSystem: 'bmap',
           data: convertData(data, geo),
           encode: {
             value: 2,
           },
           itemStyle: {
-            color: "purple",
+            color: 'purple',
           },
           symbolSize: (val) => {
-            return val[2] / 10;
+            return val[2] / 10
           },
           label: {
             show: false,
             formatter: `{b}`,
-            position: "right",
+            position: 'right',
           },
           emphasis: {
             label: {
@@ -67,9 +66,9 @@ export default {
           },
         },
         {
-          name: "top 10",
-          type: "effectScatter",
-          coordinateSystem: "bmap",
+          name: 'top 10',
+          type: 'effectScatter',
+          coordinateSystem: 'bmap',
           data: convertData(
             data.sort((a, b) => b.value - a.value),
             geo
@@ -78,19 +77,19 @@ export default {
             value: 2,
           },
           itemStyle: {
-            color: "purple",
+            color: 'purple',
             shadowBlur: 10,
-            shadowColor: "#333",
+            shadowColor: '#333',
           },
           symbolSize: (val) => {
-            return val[2] / 10;
+            return val[2] / 10
           },
           label: {
             show: false,
             formatter: (v) => {
-              return `${v.data.name} - ${v.data.value[2]}`;
+              return `${v.data.name} - ${v.data.value[2]}`
             },
-            position: "right",
+            position: 'right',
           },
           hoverAnimation: true,
           emphasis: {
@@ -99,19 +98,19 @@ export default {
             },
           },
           rippleEffect: {
-            brushType: "stroke",
+            brushType: 'stroke',
           },
         },
       ],
       title: {
-        text: "销售数据大盘",
-        subtext: "销售趋势统计",
-        sublink: "https://www.baidu.com",
-        left: "center",
+        text: '销售数据大盘',
+        subtext: '销售趋势统计',
+        sublink: 'https://www.baidu.com',
+        left: 'center',
       },
-    };
+    }
   },
-};
+}
 </script>
 <style>
 html,
