451279a8e62d6a96bb59e285ad54d780804edd62
miaoyu <miaoyu@sefonsoft.com>
2021-01-12 12:03:54 +0800

    modify

--- src/components/BMapScatter/index.vue
+++ src/components/BMapScatter/index.vue
@@ -1,184 +1,64 @@
 <template>
   <ve-bmap
-    :setting="chartSettings"
-    :title="title"
-    :tooltip="chartTooltip"
+    :settings="chartSettings"
     :series="chartSeries"
+    :tooltip="chartTooltip"
+    :title="title"
     height="100%"
-  />
+  >
+  </ve-bmap>
 </template>
 
 <script>
-import { data, geo } from './data'
+import { data, geo, styleJson } from "./data";
 const convertData = function (data, geo) {
-  const res = []
+  const res = [];
   data.forEach((item) => {
-    const { name, value } = item
-    const coord = geo[name]
+    const { name, value } = item;
+    const coord = geo[name];
     res.push({
       name,
       value: [...coord, value],
-    })
-  })
-  return res
-}
+    });
+  });
+  return res;
+};
 
-/* eslint-disable */
 export default {
   data() {
+    this.chartTooltip = { show: true };
     return {
-      title: {
-        text: '外卖销售数据大盘',
-        subtext: '销售趋势统计',
-        sublink: 'https://www.baidu.com',
-        left: 'center',
-      },
-      chartTooltip: {},
       chartSettings: {
-        key: 'oBvDtR6nzWtVchkY4cLHtnah1VVZQKR',
+        key: "oBvDtR6nzWtVchkY4cLHtnah1VVZQKRK",
         bmap: {
           center: [104.114129, 37.550339],
           zoom: 5,
-          roam: false,
+          roam: true,
           mapStyle: {
-            styleJson: [
-              {
-                featureType: 'water',
-                elementType: 'all',
-                stylers: {
-                  color: '#d1d1d1',
-                },
-              },
-              {
-                featureType: 'land',
-                elementType: 'all',
-                stylers: {
-                  color: '#f3f3f3',
-                },
-              },
-              {
-                featureType: 'railway',
-                elementType: 'all',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'highway',
-                elementType: 'all',
-                stylers: {
-                  color: '#fdfdfd',
-                },
-              },
-              {
-                featureType: 'highway',
-                elementType: 'labels',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'arterial',
-                elementType: 'geometry',
-                stylers: {
-                  color: '#fefefe',
-                },
-              },
-              {
-                featureType: 'arterial',
-                elementType: 'geometry.fill',
-                stylers: {
-                  color: '#fefefe',
-                },
-              },
-              {
-                featureType: 'poi',
-                elementType: 'all',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'green',
-                elementType: 'all',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'subway',
-                elementType: 'all',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'manmade',
-                elementType: 'all',
-                stylers: {
-                  color: '#d1d1d1',
-                },
-              },
-              {
-                featureType: 'local',
-                elementType: 'all',
-                stylers: {
-                  color: '#d1d1d1',
-                },
-              },
-              {
-                featureType: 'arterial',
-                elementType: 'labels',
-                stylers: {
-                  visibility: 'off',
-                },
-              },
-              {
-                featureType: 'boundary',
-                elementType: 'all',
-                stylers: {
-                  color: '#fefefe',
-                },
-              },
-              {
-                featureType: 'building',
-                elementType: 'all',
-                stylers: {
-                  color: '#d1d1d1',
-                },
-              },
-              {
-                featureType: 'label',
-                elementType: 'labels.text.fill',
-                stylers: {
-                  color: '#999999',
-                },
-              },
-            ],
+            styleJson,
           },
         },
       },
+      chartTooltip: {},
       chartSeries: [
         {
-          name: '销售额',
-          type: 'scatter',
-          coordinateSystem: 'bmap',
+          name: "销售额",
+          type: "scatter",
+          coordinateSystem: "bmap",
           data: convertData(data, geo),
           encode: {
             value: 2,
           },
           itemStyle: {
-            color: 'purple',
+            color: "purple",
           },
-          symbolSize: function (val) {
-            return val[2] / 10
+          symbolSize: (val) => {
+            return val[2] / 10;
           },
           label: {
             show: false,
-            position: 'right',
-            formatter: function (v) {
-              return `${v.data.name} - ${v.data.value[2]}`
-            },
+            formatter: `{b}`,
+            position: "right",
           },
           emphasis: {
             label: {
@@ -187,40 +67,58 @@ export default {
           },
         },
         {
-          name: 'Top 10',
-          type: 'effectScatter',
-          coordinateSystem: 'bmap',
+          name: "top 10",
+          type: "effectScatter",
+          coordinateSystem: "bmap",
           data: convertData(
-            data.sort(function (a, b) {
-              return b.value - a.value
-            }),
+            data.sort((a, b) => b.value - a.value),
             geo
           ).slice(0, 10),
-          symbolSize: function (val) {
-            return val[2] / 10
-          },
           encode: {
             value: 2,
           },
+          itemStyle: {
+            color: "purple",
+            shadowBlur: 10,
+            shadowColor: "#333",
+          },
+          symbolSize: (val) => {
+            return val[2] / 10;
+          },
           label: {
-            formatter: function (v) {
-              return `${v.data.name} - ${v.data.value[2]}`
+            show: false,
+            formatter: (v) => {
+              return `${v.data.name} - ${v.data.value[2]}`;
             },
-            position: 'right',
-            show: true,
+            position: "right",
           },
           hoverAnimation: true,
-          rippleEffect: {
-            brushType: 'stroke',
+          emphasis: {
+            label: {
+              show: true,
+            },
           },
-          itemStyle: {
-            color: 'purple',
-            shadowBlur: 10,
-            shadowColor: '#333',
+          rippleEffect: {
+            brushType: "stroke",
           },
         },
       ],
-    }
+      title: {
+        text: "销售数据大盘",
+        subtext: "销售趋势统计",
+        sublink: "https://www.baidu.com",
+        left: "center",
+      },
+    };
   },
-}
+};
 </script>
+<style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
