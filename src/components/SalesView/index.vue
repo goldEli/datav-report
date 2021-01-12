cbdeac69882fe7e68c0257f9753a31cff5679bef
miaoyu <miaoyu@sefonsoft.com>
2021-01-12 19:05:13 +0800

    sales view api

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,49 +1,48 @@
 <template>
   <div class="sales-view">
-    <el-card shadow="hover" :body-style="{ padding: 0 }">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
       <template v-slot:header>
         <div class="menu-wrapper">
           <el-menu
-            @select="onMenuSelect"
             :default-active="activeIndex"
             mode="horizontal"
+            @select="onMenuSelect"
             class="sales-view-menu"
           >
-            <el-menu-item index="1">sales</el-menu-item>
-            <el-menu-item index="2">visits</el-menu-item>
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
           </el-menu>
           <div class="menu-right">
-            <el-radio-group size="small" v-model="radioSelect">
-              <el-radio-button label="Today" />
-              <el-radio-button label="Week" />
-              <el-radio-button label="Month" />
-              <el-radio-button label="Year" />
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日" />
+              <el-radio-button label="本周" />
+              <el-radio-button label="本月" />
+              <el-radio-button label="今年" />
             </el-radio-group>
             <el-date-picker
-              v-model="date"
               type="daterange"
-              start-placeholder="start"
-              end-placeholder="end"
+              v-model="date"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
               size="small"
-              class="sales-view-date-picker"
               unlink-panels
               :picker-options="pickerOptions"
-            ></el-date-picker>
+              class="sales-view-date-picker"
+            />
           </div>
         </div>
       </template>
       <template>
         <div class="sales-view-chart-wrapper">
-          <v-chart :options="chartOptions" />
+          <v-chart :options="chartOption" />
           <div class="sales-view-list">
-            <div class="sales-view-list-title">Ranking</div>
+            <div class="sales-view-title">排行榜</div>
             <div class="list-item-wrapper">
               <div class="list-item" v-for="item in rankData" :key="item.no">
-                <div :class="['list-item-no', item.no <= 3 ? 'top-no' : '']">
-                  {{ item.no }}
-                </div>
-                <div class="list-item-name">{{ item.name }}</div>
-                <div class="list-item-money">{{ item.money }}</div>
+                <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{item.no}}</div>
+                <div class="list-item-name">{{item.name}}</div>
+                <div class="list-item-money">{{item.money}}</div>
               </div>
             </div>
           </div>
@@ -54,227 +53,220 @@
 </template>
 
 <script>
-function onSetDate(picker, number) {
-  const start = new Date()
-  const end = new Date()
-  start.setTime(start.getTime() - 3600 * 24 * 1000 * number)
-  picker.$emit('pick', [start, end])
-}
-export default {
-  data() {
-    return {
-      activeIndex: '1',
-      radioSelect: 'Today',
-      date: null,
-      pickerOptions: {
-        shortcuts: [
-          {
-            text: 'last week',
+  import commonDataMixin from '../../mixins/commonDataMixin'
+
+  export default {
+    mixins: [commonDataMixin],
+    data() {
+      return {
+        activeIndex: '1',
+        radioSelect: '今日',
+        date: null,
+        pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
             onClick(picker) {
-              onSetDate(picker, 7)
-            },
-          },
-          {
-            text: 'last month',
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近一个月',
             onClick(picker) {
-              onSetDate(picker, 30)
-            },
-          },
-          {
-            text: 'last three months',
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近三个月',
             onClick(picker) {
-              onSetDate(picker, 30 * 3)
-            },
-          },
-        ],
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          }]
+        },
+        chartOption: {}
+      }
+    },
+    computed: {
+      rankData() {
+        return this.activeIndex === '1' ? this.orderRank : this.userRank
+      }
+    },
+    watch: {
+      orderFullYear() {
+        this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
+      }
+    },
+    methods: {
+      onMenuSelect(index) {
+        this.activeIndex = index
+        if (index === '1') {
+          this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
+        } else {
+          this.render(this.userFullYear, this.userFullYearAxis, '年度用户访问量')
+        }
       },
-      chartOptions: {
-        title: {
-          text: 'Sales',
-          textStyle: {
-            fontSize: 12,
-            color: '#666',
-          },
-          left: 25,
-          top: 20,
-        },
-        xAxis: {
-          type: 'category',
-          data: [
-            '1月',
-            '2月',
-            '3月',
-            '4月',
-            '5月',
-            '6月',
-            '7月',
-            '8月',
-            '9月',
-            '10月',
-            '11月',
-            '12月',
-          ],
-          axisTick: {
-            alignWithLabel: true,
-            lineStyle: {
-              color: '#999',
+      render(data, axis, title) {
+        this.chartOption = {
+          title: {
+            text: title,
+            textStyle: {
+              fontSize: 12,
+              color: '#666'
             },
+            left: 25,
+            top: 20
           },
-          axisLine: {
-            lineStyle: {
-              color: '#999',
+          xAxis: {
+            type: 'category',
+            data: axis,
+            axisTick: {
+              alignWithLabel: true,
+              lineStyle: {
+                color: '#999'
+              }
             },
-          },
-          axisLabel: {
-            color: '#333',
-          },
-        },
-        yAxis: {
-          axisLine: {
-            show: false,
-          },
-          axisTick: {
-            show: false,
-          },
-          splitLine: {
-            lineStyle: {
-              type: 'dotted',
-              color: '#eee',
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
             },
+            axisLabel: {
+              color: '#333'
+            }
           },
-        },
-        series: [
-          {
+          yAxis: {
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              lineStyle: {
+                type: 'dotted',
+                color: '#eee'
+              }
+            }
+          },
+          series: [{
             type: 'bar',
             barWidth: '35%',
-            data: [123, 345, 234, 123, 345, 567, 345, 234, 123, 345, 234, 234],
-          },
-        ],
-        color: ['#3398DB'],
-        grid: {
-          top: 70,
-          left: 60,
-          right: 60,
-          bottom: 50,
-        },
-      },
-      rankData: [
-        {
-          no: 1,
-          name: 'McDonald\'s',
-          money: '323,234',
-        },
-        {
-          no: 2,
-          name: 'KFC',
-          money: '323,234',
-        },
-        {
-          no: 3,
-          name: 'Dex',
-          money: '323,234',
-        },
-        {
-          no: 4,
-          name: 'Pizza Hut',
-          money: '323,234',
-        },
-      ],
+            data
+          }],
+          color: ['#3398DB'],
+          grid: {
+            top: 70,
+            left: 60,
+            right: 60,
+            bottom: 50
+          }
+        }
+      }
     }
-  },
-  methods: {
-    onMenuSelect(index) {
-      this.activeIndex = index
-    },
-  },
-}
+  }
 </script>
 
 <style lang="scss" scoped>
-.sales-view {
-  margin-top: 20px;
-  .menu-wrapper {
-    position: relative;
-    display: flex;
-    .sales-view-menu {
-      width: 100%;
-      padding-left: 20px;
-      .el-menu-item {
-        height: 50px;
-        line-height: 50px;
-        margin: 0 20px;
-      }
-    }
-    .menu-right {
-      position: absolute;
-      top: 0;
-      right: 0;
-      height: 50px;
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      .sales-view-date-picker {
-        margin-left: 20px;
-      }
-    }
-  }
-  .sales-view-chart-wrapper {
-    display: flex;
-    height: 270px;
-    .echarts {
-      width: 70%;
-      height: 100%;
-      flex: 0, 0, 70%;
-    }
-  }
-  .sales-view-list {
-    flex: 1;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    .sales-view-list-title {
-      margin-top: 20px;
-      font-size: 12px;
-      color: #666;
-      font-weight: 500;
-    }
-    .list-item-wrapper {
-      margin-top: 15px;
+  .sales-view {
+    margin-top: 20px;
 
-      .list-item {
+    .menu-wrapper {
+      position: relative;
+      display: flex;
+
+      .sales-view-menu {
+        width: 100%;
+        padding-left: 20px;
+
+        .el-menu-item {
+          height: 50px;
+          line-height: 50px;
+          margin: 0 20px;
+        }
+      }
+
+      .menu-right {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        height: 50px;
         display: flex;
         align-items: center;
-        font-size: 12px;
-        height: 20px;
-        padding: 6px 20px 6px 0;
+        justify-content: flex-end;
 
-        .list-item-no {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          width: 20px;
-          height: 20px;
-          color: #333;
+        .sales-view-date-picker {
+          margin-left: 20px;
+        }
+      }
+    }
 
-          &.top-no {
-            background: #000;
-            border-radius: 50%;
-            color: #fff;
-            font-weight: 500;
+    .sales-view-chart-wrapper {
+      display: flex;
+      height: 270px;
+
+      .echarts {
+        flex: 0 0 70%;
+        width: 70%;
+        height: 100%;
+      }
+
+      .sales-view-list {
+        flex: 1;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+
+        .sales-view-title {
+          margin-top: 20px;
+          font-size: 12px;
+          color: #666;
+          font-weight: 500;
+        }
+
+        .list-item-wrapper {
+          margin-top: 15px;
+
+          .list-item {
+            display: flex;
+            align-items: center;
+            font-size: 12px;
+            height: 20px;
+            padding: 6px 20px 6px 0;
+
+            .list-item-no {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              width: 20px;
+              height: 20px;
+              color: #333;
+
+              &.top-no {
+                background: #000;
+                border-radius: 50%;
+                color: #fff;
+                font-weight: 500;
+              }
+            }
+
+            .list-item-name {
+              margin-left: 10px;
+              color: #333;
+            }
+
+            .list-item-money {
+              flex: 1;
+              text-align: right;
+            }
           }
-        }
-
-        .list-item-name {
-          margin-left: 10px;
-          color: #333;
-        }
-
-        .list-item-money {
-          flex: 1;
-          text-align: right;
         }
       }
     }
   }
-}
 </style>
